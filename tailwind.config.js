5a059c35877cd48b96b3c97b50e3869b00e3ce3c
leasben <simbaleee123@gmail.com>
2024-10-21 16:55:31 +0200

    schools section added

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,7 +25,7 @@ module.exports = {
         transparent: 'transparent',
         current: 'currentColor',
         blue: '#032765',
-        lightBlue: '#003DA6',
+        lightBlue: '#2961e5',
       },
       boxShadow: {
         'button': '2px 8px 4px -2px rgba(226, 226, 226, 0.75)',
